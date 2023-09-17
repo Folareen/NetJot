20946088c1f07cc858a57e4af14bfe354f021117
Folareen <sakawahab03@gmail.com>
2023-09-17 23:25:10 +0100

    cleanup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>NetJot!</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+div {
+  font-family: sans-serif;
+  color: blue;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 1.5em;
 }
 </style>
