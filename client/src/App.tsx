3257a2254c54a2fda90439383d3bf2e7f73971e4
Folareen <sakawahab03@gmail.com>
2025-01-02 13:00:08 +0100

    setup

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <div>Netjot</div>
+  )
+}
+
+export default App
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,9 +2,8 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ], theme: {
     extend: {},
   },
   plugins: [],
